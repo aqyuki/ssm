6ec75e128b7689cc5d97cf248711e4471b1e9df6
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-21 14:43:32 +0900

    chore : 設定の読み込み機能を追加

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/aqyuki/ssm/commands"
+import (
+	"os"
+
+	"github.com/aqyuki/ssm/commands"
+	"github.com/aqyuki/ssm/config"
+)
 
 func main() {
+
+	// アプリケーションの初期化
+	_, err := config.Initialize()
+	if err != nil {
+		os.Exit(-1)
+	}
+
 	if err := commands.RootCmd.Execute(); err != nil {
-		return
+		os.Exit(-1)
 	}
 }
