c7200c1ce2d663616fbffb51d9cf2f612fd14ee6
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-21 17:28:14 +0900

    feat : サポートしていないOSで起動したときエラーメッセージを表示するように変更

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/aqyuki/ssm/commands"
@@ -12,6 +14,10 @@ func main() {
 	// アプリケーションの初期化
 	_, err := config.Initialize()
 	if err != nil {
+		if errors.Is(err, config.ErrUnsupportedOS) {
+			fmt.Printf("This application supports Windows only\n")
+			os.Exit(-1)
+		}
 		os.Exit(-1)
 	}
 
